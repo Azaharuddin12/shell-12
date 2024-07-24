76dd2f3054723d08e5547f61cf42a98496ff9bca
Azaharuddin12 <azaharuddin12.10@gmail.com>
2024-07-24 21:26:33 +0530

    user promt

---
+++ 04-arry.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+MOVIES=("xyz" "abc" "cdf")
+# size  of arry 3 
+#index 0,1,2 
+
+# this list always starts with 0
+
+echo "first movie is : ${MOVIES[0]}"
+
+echo "second movie is : ${MOVIES[1]}"
+
+echo "third movie is : ${MOVIES[2]}"
+
+echo "all movie is : ${MOVIES[@]}"
+
