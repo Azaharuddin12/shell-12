a2696064b8241705b29a6dc414bc9529b25d4df3
Azaharuddin12 <azaharuddin12.10@gmail.com>
2024-07-24 21:01:08 +0530

    variables added

--- 12-conversation.sh
+++ 12-conversation.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo "Azar:hi rajesh bro"
-echo "Rajesh:ya! azar is there anything intresting"
-echo "Azar:yes bro,i have come up with a new workout plan for our gym routine."
-echo "Rajesh:hey! its a good thing azar,is there anything new in the schedule."
-echo "Azar:yes bro, i have arranged some raft of time for HIIT ana abs workouts"
-echo "Rajesh:hey! it's a good thing azar we this plan from tommorow onwards"
-echo "Azar: ya! bro for sure."  
+PERSON1=azar
+PERSON2=Rajesh
+
+
+echo "$PERSON1:hi $PERSON2 bro"
+echo "$PERSON2:ya! $PERSON1 is there anything intresting"
+echo "$PERSON1:yes bro,i have come up with a new workout plan for our gym routine."
+echo "$PERSON2:hey! its a good thing $PERSON1,is there anything new in the schedule."
+echo "$PERSON1:yes bro, i have arranged some raft of time for HIIT ana abs workouts"
+echo "$PERSON2:hey! it's a good thing $PERSON1 we this plan from tommorow onwards"
+echo "$PERSON1: ya! bro for sure."  
+
