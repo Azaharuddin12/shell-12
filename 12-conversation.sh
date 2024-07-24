be849660f657c3e03f13b2047a74288e822f536e
Azaharuddin12 <azaharuddin12.10@gmail.com>
2024-07-24 17:55:47 +0530

    our convo

---
+++ 12-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Azar:hi rajesh bro"
+echo "Rajesh:ya! azar is there anything intresting"
+echo "Azar:yes bro,i have come up with a new workout plan for our gym routine."
+echo "Rajesh:hey! its a good thing azar,is there anything new in the schedule."
+echo "Azar:yes bro, i have arranged some raft of time for HIIT ana abs workouts"
+echo "Rajesh:hey! it's a good thing azar we this plan from tommorow onwards"
+echo "Azar: ya! bro for sure."  
